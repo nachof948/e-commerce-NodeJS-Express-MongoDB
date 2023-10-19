ea470c9c3e7e7bef4f696c105caf85b41a3412af
nachof948 <nachof948@gmail.com>
2023-10-19 14:38:41 -0300

    Avances en el proyecto final

---
+++ public/js/swiper.js
@@ -0,0 +1,58 @@
+const swiperHero = new Swiper('.swiper-especial', {
+    // Optional parameters
+      direction: 'horizontal',
+      loop: true,
+      effect: 'fade',
+      autoplay: {
+        delay:3000,
+        pauseOnMouseEnter:true,
+        disableOnInteraction:false,
+      },
+  
+    // If we need pagination
+      pagination: {
+      el: '.swiper-pagination',
+      clickable:true,
+      },
+  
+    // Navigation arrows
+  /*     navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+      }, */
+  
+    // And if we need scrollbar
+  /*     scrollbar: {
+      el: '.swiper-scrollbar',
+      draggable: true,
+      }, */
+  });
+  const swiper = new Swiper('.swiper-habitual', {
+    // Optional parameters
+      direction: 'horizontal',
+      loop: true,
+      effect: 'fade',
+      autoplay: {
+        delay:3000,
+        pauseOnMouseEnter:true,
+        disableOnInteraction:false,
+      },
+  
+    // If we need pagination
+      pagination: {
+      el: '.swiper-pagination',
+      clickable:true,
+      },
+  
+    // Navigation arrows
+  /*     navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+      }, */
+  
+    // And if we need scrollbar
+  /*     scrollbar: {
+      el: '.swiper-scrollbar',
+      draggable: true,
+      }, */
+  });
