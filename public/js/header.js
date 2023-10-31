65e6a8ce9c435820cfc24f544dfe9a4ff6895818
nachof948 <nachof948@gmail.com>
2023-10-30 23:50:10 -0300

    Avances en el diseño responsive

--- public/js/header.js
+++ public/js/header.js
@@ -1,5 +1,11 @@
 const header = document.getElementById('header');
 const contenedorHeader = document.getElementsByClassName('contenedor-header');
+const buttonResponsive = document.getElementById("menuResponsive");
+const menuResponsive = document.querySelector('.contenedor-menu-responsive');
+
+buttonResponsive.addEventListener('click',()=>{
+    menuResponsive.classList.toggle('activo')
+})
 
 
 document.querySelectorAll('.tarjeta-producto').forEach(tarjeta => {
@@ -10,11 +16,13 @@ document.querySelectorAll('.tarjeta-producto').forEach(tarjeta => {
   });
 
 
-window.addEventListener('scroll', () =>{
-    if(window.scrollY > 1){
-        header.style.backgroundColor = 'rgb(132,183,78)';
-        header.style.padding= '0rem 0rem';
-    }else{
-        header.style.backgroundColor = 'transparent';
+  window.addEventListener('scroll', () => {
+    if (window.innerWidth > 1024) {
+        if (window.scrollY > 1) {
+            header.style.backgroundColor = 'rgb(132,183,78)';
+            header.style.padding = '0rem 0rem';
+        } else {
+            header.style.backgroundColor = 'transparent';
+        }
     }
 })
