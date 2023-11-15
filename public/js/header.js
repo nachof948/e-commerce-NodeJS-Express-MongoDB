68377f75b544df84a8d38e60a90456d07aaae24a
nachof948 <nachof948@gmail.com>
2023-11-15 09:12:15 -0300

    Entrega final

--- public/js/header.js
+++ public/js/header.js
@@ -4,12 +4,9 @@ const buttonResponsive = document.getElementById("menuResponsive");
 const menuResponsive = document.querySelector('.contenedor-menu-responsive');
 const btnSesion = document.querySelector('.perfil')
 const cerrarSesion = document.querySelector('.cerrar-sesion');
+const enlacesMenu = document.querySelectorAll('.contenedor-menu-responsive a');
 
-
-buttonResponsive.addEventListener('click',()=>{
-    menuResponsive.classList.toggle('activo')
-})
-
+/* HACER CLICK EN UNA TARJETA Y QUE MUESTRE LOS DETALLES DEL PRODUCTO */
 document.querySelectorAll('.tarjeta-producto').forEach(tarjeta => {
     tarjeta.addEventListener('click', function() {
       const productoId = this.dataset.productoId; // Obtener el ID del producto de la tarjeta clickeada
@@ -19,8 +16,19 @@ document.querySelectorAll('.tarjeta-producto').forEach(tarjeta => {
             window.location.href = '/auth/signup'
         }
     });
-  });
+});
 
+/* MENU RESPONSIVE */
+buttonResponsive.addEventListener('click',()=>{
+    menuResponsive.classList.toggle('activo')
+})
+enlacesMenu.forEach(enlace => {
+    enlace.addEventListener('click', () => {
+        menuResponsive.classList.remove('activo');
+    });
+});
+
+/* MODIFICAR UN ESTILO DEL HEADER */
 window.addEventListener('scroll', () => {
     if (window.innerWidth > 1024) {
         if (window.scrollY > 1) {
@@ -31,6 +39,7 @@ window.addEventListener('scroll', () => {
         }
     }
 })
+/* BOTON CERRAR SESION */
 btnSesion.addEventListener('click',()=>{
     if (cerrarSesion.style.display === 'block') {
         cerrarSesion.style.display = 'none';
